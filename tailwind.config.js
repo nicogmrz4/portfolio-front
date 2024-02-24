8c90c493e97002b4dfa9769dc770f20f8a048d82
Nicolas <nicolasgalarza40@gmail.com>
2024-02-24 15:15:57 -0300

    add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,24 @@ module.exports = {
   ],
   prefix: 'tw-',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'persian-blue': {
+          '50': '#ecf4ff',
+          '100': '#ddebff',
+          '200': '#c2daff',
+          '300': '#9dc1ff',
+          '400': '#769cff',
+          '500': '#5678fe',
+          '600': '#384ff3',
+          '700': '#2637c9',
+          '800': '#2636ad',
+          '900': '#273588',
+          '950': '#171e4f',
+      },
+      
+      }
+    },
     screens: {
       sm: '480px',
       md: '768px',
