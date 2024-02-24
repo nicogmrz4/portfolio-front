d74fc876d17d313a802c3d6cd9fc5bce8cef63f0
Nicolas <nicolasgalarza40@gmail.com>
2024-02-24 15:15:41 -0300

    reorganize folder structure

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -4,12 +4,12 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { SharedModule } from '@shared/shared.module';
-import { MeComponent } from './components/sections/me/me.component';
+import { MeComponent } from './sections/me/me.component';
 import { ProjectCardComponent } from './components/project-card/project-card.component';
 import { SkillCardComponent } from './components/skill-card/skill-card.component';
-import { ProjectsComponent } from './components/sections/projects/projects.component';
-import { SkillsComponent } from './components/sections/skills/skills.component';
-import { ContactComponent } from './components/sections/contact/contact.component';
+import { ProjectsComponent } from './sections/projects/projects.component';
+import { SkillsComponent } from './sections/skills/skills.component';
+import { ContactComponent } from './sections/contact/contact.component';
 
 @NgModule({
   declarations: [
