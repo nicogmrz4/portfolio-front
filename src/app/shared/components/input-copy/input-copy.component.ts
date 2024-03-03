8a78b45743411fa5bb221601f73936a86a6c6a90
Nicolas <nicolasgalarza40@gmail.com>
2024-03-02 21:14:33 -0300

    Tooltip added on click copy button

--- src/app/shared/components/input-copy/input-copy.component.ts
+++ src/app/shared/components/input-copy/input-copy.component.ts
@@ -1,18 +1,29 @@
+import { AsyncPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-input-copy',
   templateUrl: './input-copy.component.html',
   styleUrl: './input-copy.component.scss',
-  standalone: true
+  standalone: true,
+  imports: [ AsyncPipe ]
 })
 export class InputCopyComponent {
   disabled: boolean = true;
+  toolTip: boolean = false;
+  toolTip$: BehaviorSubject<boolean> = new BehaviorSubject(false);
 
   copyInputEmail(inputElement: HTMLInputElement) {
     inputElement.disabled = false;
     inputElement.select();
     document.execCommand('copy');
     inputElement.disabled = true;
+    this.showTooltip();
+  }
+  
+  showTooltip() {
+    this.toolTip$.next(true);
+    setTimeout(() => this.toolTip$.next(false), 500);  
   }
 }
