c059633bfaefffe517be2b187872eeff7b679f70
Nicolas <nicolasgalarza40@gmail.com>
2024-03-12 11:56:42 -0300

    Snackbar services created

---
+++ src/app/modules/dashboard/services/snackbar.service.ts
@@ -0,0 +1,13 @@
+import { Injectable, inject } from '@angular/core';
+import { MatSnackBar, MatSnackBarConfig } from '@angular/material/snack-bar'; 
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SnackbarService {
+  private readonly snackbar: MatSnackBar = inject(MatSnackBar);
+
+  open(message: string, action: string = 'Ok', config: MatSnackBarConfig = {}) {
+    this.snackbar.open(message, action, config);
+  }
+}
