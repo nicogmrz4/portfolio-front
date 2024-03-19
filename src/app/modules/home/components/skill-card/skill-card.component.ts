e62daa07fef87b7bc79f87fd25905daca3d0b780
Nicolas <nicolasgalarza40@gmail.com>
2024-03-19 02:57:56 -0300

    fix enviroment

--- src/app/modules/home/components/skill-card/skill-card.component.ts
+++ src/app/modules/home/components/skill-card/skill-card.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { environment } from '../../../../../environments/environment.development';
+import { environment } from '../../../../../environments/environment';
 
 @Component({
   selector: 'app-skill-card',
